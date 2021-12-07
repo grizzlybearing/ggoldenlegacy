9281c27718714fd1ae003a031b8e55ae5c71c291
Ann <xhjfurb11@gmail.com>
2021-12-07 23:16:50 +0300

    newl

---
+++ src/main/java/com/legacy/test/Calculator.java
@@ -0,0 +1,18 @@
+package com.legacy.test;
+
+public class Calculator {
+    public int addTwoNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public int substractTwoNumbers(int a, int b) {
+        return a - b;
+    }
+
+    public int miltiplicateTwoNumbers(int a, int b){
+        return a * b;}
+
+    public double divideTwoNumbers(double a, double b){
+        return a / b;}
+
+}
